d6cf65ad83927700c6ac3eeb7cea8285a39afc85
Radoslaw Szymkiewicz <szymkiewicz.radoslaw@gmail.com>
2025-05-27 16:02:33 +0200

    Init

---
+++ README.md
@@ -0,0 +1,183 @@
+# Medusa Paypal Payment
+
+## What is it?
+
+Medusa Paypal Payment is a basic integration of payment provider for Paypal.
+
+## Installation
+
+1. Install plugin by adding to your `package.json`:
+
+**Warning**
+
+```json
+...
+"@rsc-labs/medusa-paypal-payment": "0.0.1" // or other available version
+...
+```
+and execute install, e.g. `yarn install`.
+
+2. Add plugin to your `medusa-config.js` (**Note** - please notice that you need to add it to payment plugin):
+
+```js
+...
+  plugins: [
+    {
+      resolve: "@rsc-labs/medusa-paypal-payment",
+      options: {
+        token: <app-token>,
+        environment: <env-definition>,
+      },
+    }
+  ],
+  modules: [
+    {
+      resolve: "@medusajs/medusa/payment",
+      options: {
+        providers: [
+          {
+            resolve: "@rsc-labs/medusa-paypal-payment/providers/paypal-payment",
+            id: "paypal-payment",
+            options: {
+              oAuthClientId: <oauth-client-id>,
+              oAuthClientSecret: <oauth-client-secret>,
+              environment: <env-definition>,
+            },
+          }
+        ]
+      },
+    },
+...
+```
+
+## Overview
+
+The Paypal Provider gives ability to:
+- make a payment using Paypal
+- cancel payment
+- refund payment
+- track payments in Paypal
+
+## Configuration
+
+Plugin uses 3 required parameters:
+
+- `oAuthClientId` - required parameter which you can find in your Paypal Developer Dashboard
+- `oAuthClientSecret` - required parameter which you can find in your Paypal Developer Dashboard
+- `environment` - set to `sandbox` or `production`. You can use it to test with your `sandbox` environment.
+
+
+After above configuration, you can then add the payment provider to your reqion.
+
+## Storefront
+
+We recommend using `@paypal/react-paypal-js` package on your storefront as it simplifies the implementation a lot.
+Here is the example of using Paypal as payment:
+
+```tsx
+import { OnApproveActions, OnApproveData } from "@paypal/paypal-js"
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js"
+...
+const PayPalPaymentButton = ({
+  cart,
+  notReady,
+  "data-testid": dataTestId,
+}: {
+  cart: HttpTypes.StoreCart
+  notReady: boolean
+  "data-testid"?: string
+}) => {
+  const [submitting, setSubmitting] = useState(false)
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
+
+  const onPaymentCompleted = async () => {
+    await placeOrder()
+      .catch((err) => {
+        setErrorMessage(err.message)
+      })
+      .finally(() => {
+        setSubmitting(false)
+      })
+  }
+
+  const session = cart.payment_collection?.payment_sessions?.find(
+    (s) => s.status === "pending"
+  )
+
+  const handlePayment = async (
+    _data: OnApproveData,
+    actions: OnApproveActions
+  ) => {
+    actions?.order
+      ?.authorize()
+      .then((authorization) => {
+        if (authorization.status !== "COMPLETED") {
+          setErrorMessage(`An error occurred, status: ${authorization.status}`)
+          return
+        }
+        onPaymentCompleted()
+      })
+      .catch((error) => {
+        setErrorMessage(`An unknown error occurred, please try again.`)
+        setSubmitting(false)
+      })
+  }
+
+  const [{ isPending, isResolved }] = usePayPalScriptReducer()
+
+  if (isPending) {
+    return <Spinner />
+  }
+
+  if (isResolved) {
+    return (
+      <>
+        <PayPalButtons
+          style={{ layout: "horizontal" }}
+          createOrder={async () => {
+            return session?.data.paypalOrderId as string;
+          }}
+          onApprove={handlePayment}
+          disabled={notReady || submitting || isPending}
+          data-testid={dataTestId}
+        />
+        <ErrorMessage
+          error={errorMessage}
+          data-testid="paypal-payment-error-message"
+        />
+      </>
+    )
+  }
+}
+...
+
+// Please remember that above PaypalButton needs to be a child of PaypalScriptProvider
+
+return (<PayPalScriptProvider
+    options={{
+      "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || "test",
+      currency: cart?.currency_code.toUpperCase(),
+      intent: "authorize",
+      components: "buttons",
+    }}
+  >
+    {children}
+  </PayPalScriptProvider>
+)
+```
+
+`client-id` - you can retrieve it from your Paypal Developer Dashboard.
+
+### Notes
+
+1. Intent has been chosen to `authorize` in `PayPalScriptProvider` - it means that firstly payment is authorized, then it can be capture via Admin UI. Plugin (not yet) support automatic capturing.
+2. `usePayPalScriptReducer` requires `PayPalScriptProvider` to be a parent. It is not problem when making a payment, but when you would like to redirect the user you need be careful. The best option is to put `PayPalScriptProvider` as high as possible in components' tree.
+3. `session?.data.paypalOrderId` - `paypalOrderId` is created by the plugin and put it in the `data`. However, there are more information put in `data` by the plugin, so you can log them into console and see how you can use it in your storefront.
+
+## License
+
+MIT
+
+---
+
+© 2025 RSC https://rsoftcon.com/
---
+++ src/providers/paypal-payment/service.ts
@@ -0,0 +1,326 @@
+import { AbstractPaymentProvider, PaymentActions } from "@medusajs/framework/utils"
+import { AuthorizePaymentInput, AuthorizePaymentOutput, BigNumberRawValue, CancelPaymentInput, CancelPaymentOutput, CapturePaymentInput, CapturePaymentOutput, DeletePaymentInput, DeletePaymentOutput, GetPaymentStatusInput, GetPaymentStatusOutput, InitiatePaymentInput, InitiatePaymentOutput, Logger, PaymentSessionStatus, ProviderWebhookPayload, RefundPaymentInput, RefundPaymentOutput, RetrievePaymentInput, RetrievePaymentOutput, UpdatePaymentInput, UpdatePaymentOutput, WebhookActionResult } from "@medusajs/framework/types"
+import { AbstractEventBusModuleService, MedusaError } from "@medusajs/utils";
+import { 
+  OrdersController, 
+  PaymentsController,
+  CheckoutPaymentIntent, 
+  ApiError, 
+  Environment, 
+  LogLevel, 
+  Client as PaypalClient,
+  Order as PaypalOrder,
+  OrderStatus as PaypalOrderStatus,
+  PatchOp,
+} from "@paypal/paypal-server-sdk"
+
+type Options = {
+  oAuthClientId?: string,
+  oAuthClientSecret?: string,
+  environment?: string
+}
+
+type InjectedDependencies = {
+  logger: Logger,
+  event_bus: AbstractEventBusModuleService
+}
+
+class PaypalPaymentProviderService extends AbstractPaymentProvider<
+  Options
+> {
+
+  static identifier = "paypal-payment"
+
+  protected logger_: Logger
+  protected options_: Options
+  protected paypal_: PaypalClient;
+  protected eventBusService_: AbstractEventBusModuleService
+  constructor(
+    container: InjectedDependencies,
+    options: Options
+  ) {
+    super(container, options)
+
+    this.logger_ = container.logger
+    this.options_ = options
+    this.eventBusService_ = container.event_bus;
+
+    if (this.options_.oAuthClientId && this.options_.oAuthClientSecret && this.options_.environment) {
+      this.paypal_ = new PaypalClient({
+        clientCredentialsAuthCredentials: {
+          oAuthClientId: this.options_.oAuthClientId,
+          oAuthClientSecret: this.options_.oAuthClientSecret
+        },
+        timeout: 0,
+        environment: this.options_.environment === "sandbox" ? Environment.Sandbox : Environment.Production,
+        logging: {
+          logLevel: LogLevel.Info,
+          logRequest: {
+            logBody: true
+          },
+          logResponse: {
+            logHeaders: true
+          }
+        }
+      })
+    } else {
+      throw new MedusaError(
+        MedusaError.Types.INVALID_DATA,
+        "oAuthClientId, oAuthClientSecret and environment are required in the provider's options."
+      )
+    }
+  }
+
+  static validateOptions(options: Record<any, any>) {
+    if (!options.oAuthClientId) {
+      throw new MedusaError(
+        MedusaError.Types.INVALID_DATA,
+        "oAuthClientId is required in the provider's options."
+      )
+    }
+    if (!options.oAuthClientSecret) {
+      throw new MedusaError(
+        MedusaError.Types.INVALID_DATA,
+        "oAuthClientSecret is required in the provider's options."
+      )
+      
+    }
+    if (options.environment !== 'production' && options.environment !== "sandbox") {
+      throw new MedusaError(
+        MedusaError.Types.INVALID_DATA,
+        "environment needs to be either production or sandbox"
+      )
+    }
+  }
+
+  async capturePayment(paymentData: CapturePaymentInput): Promise<CapturePaymentOutput> {
+    const paypalOrder = paymentData.data!.paypalOrder as PaypalOrder;
+
+    if (paypalOrder.purchaseUnits?.length &&
+      paypalOrder.purchaseUnits[0].payments && 
+      paypalOrder.purchaseUnits[0].payments.authorizations?.length
+    ) {
+      const id = paypalOrder.purchaseUnits[0].payments!.authorizations[0].id as string;
+      try {
+        const paymentsController = new PaymentsController(this.paypal_);
+        await paymentsController.captureAuthorizedPayment({
+          authorizationId: id
+        })
+        return await this.retrievePayment(paymentData)
+      } catch (error) {
+        this.logger_.error(error);
+        throw new Error("An error occurred in capturePayment")
+      }
+    }
+    throw new Error("An error occurred in capturePayment")
+  }
+
+  async authorizePayment(paymentData: AuthorizePaymentInput): Promise<AuthorizePaymentOutput> {
+   try {
+      const status = (await this.getPaymentStatus(paymentData)).status;
+      const retrievedPayment = await this.retrievePayment(paymentData)
+      return {
+        data: {
+          ...paymentData,
+          paypalOrder: retrievedPayment.data!.paypalOrder,
+          paypalOrderId: (retrievedPayment.data!.paypalOrder! as PaypalOrder).id
+        },
+        status: status
+      }
+    } catch (error) {
+      throw new Error("Authorize payment failed")
+    }
+  }
+  async cancelPayment(paymentData: CancelPaymentInput): Promise<CancelPaymentOutput> {
+    const paypalOrder = paymentData.data!.paypalOrder as PaypalOrder;
+    if (paypalOrder.purchaseUnits?.length &&
+      paypalOrder.purchaseUnits[0].payments
+    ) {
+      const isAlreadyCanceled = paypalOrder.status === PaypalOrderStatus.Voided;
+      const isCanceledAndFullyRefund = paypalOrder.status === PaypalOrderStatus.Completed && !!paypalOrder.purchaseUnits[0].invoiceId;
+      if (isAlreadyCanceled || isCanceledAndFullyRefund) {
+        return await this.retrievePayment(paymentData)
+      }
+      const paymentsController = new PaymentsController(this.paypal_);
+      try {
+        const isAlreadyCaptured = paypalOrder.purchaseUnits.some((pu => pu.payments?.captures?.length));
+        if (isAlreadyCaptured) {
+          const payments = paypalOrder.purchaseUnits[0].payments;
+          const capturesId = payments.captures![0].id;
+          await paymentsController.refundCapturedPayment({
+            captureId: capturesId as string,
+          })  
+        } else {
+          const id = paypalOrder.purchaseUnits[0].payments!.authorizations![0].id as string;
+          await paymentsController.voidPayment({
+            authorizationId: id
+          })
+        }
+        return await this.retrievePayment(paymentData)
+      } catch (error) {
+        throw new Error("An error occurred in cancelPayment")
+      }
+    }
+    throw new Error("An error occurred in cancelPayment")
+  }
+  async initiatePayment(paymentData: InitiatePaymentInput): Promise<InitiatePaymentOutput> {
+    const ordersController = new OrdersController(this.paypal_);
+    try {
+      const { result, ...httpResponse } = await ordersController.createOrder({
+        body: {
+          intent: CheckoutPaymentIntent.Authorize,
+          purchaseUnits: [
+            {
+              amount: {
+                currencyCode: paymentData.currency_code,
+                value: paymentData.amount.toString()
+              }
+            }
+          ]
+        }
+      })
+      if (result.id) {
+        return {
+          id: result.id,
+          data: {
+            ...paymentData,
+            paypalOrderId: result.id,
+            paypalOrder: result
+          }
+        }
+      }
+    } catch (error) {
+      if (error instanceof ApiError) {
+        const { statusCode, headers } = error;
+        this.logger_.error(`Status code ${statusCode.toString()}`)
+        this.logger_.error(JSON.stringify(headers))
+      }
+    }
+    throw new Error("Initialize payment failed")
+  }
+  async deletePayment(paymentSessionData: DeletePaymentInput): Promise<DeletePaymentOutput> {
+    return paymentSessionData;
+  }
+  async getPaymentStatus(paymentData: GetPaymentStatusInput): Promise<GetPaymentStatusOutput> {
+
+    const retrievedPayment = await this.retrievePayment(
+      paymentData
+    );
+    const paypalOrder = retrievedPayment.data!.paypalOrder as PaypalOrder;
+
+    switch (paypalOrder.status) {
+      case PaypalOrderStatus.Created:
+        return {
+          status: 'pending'
+        }
+      case PaypalOrderStatus.Saved:
+      case PaypalOrderStatus.Approved:
+      case PaypalOrderStatus.PayerActionRequired:
+        return {
+          status: 'requires_more'
+        }
+      case PaypalOrderStatus.Voided:
+        return {
+          status: 'canceled'
+        }
+      case PaypalOrderStatus.Completed:
+        return {
+          status: 'authorized'
+        }
+      default:
+        return {
+          status: 'pending'
+        }
+    }
+  }
+  async refundPayment(paymentData: RefundPaymentInput): Promise<RefundPaymentOutput> {
+    const paypalOrder = paymentData.data!.paypalOrder as PaypalOrder;
+
+    if (paypalOrder.purchaseUnits?.length &&
+      paypalOrder.purchaseUnits[0].payments
+    ) {
+      const purchaseUnit = paypalOrder.purchaseUnits[0];
+      const isAlreadyCaptured = paypalOrder.purchaseUnits.some((pu => pu.payments?.captures?.length));
+      if (!isAlreadyCaptured) {
+        throw new Error("Cannot refund an uncaptured payment")
+      }
+
+      const paymentId = purchaseUnit.payments?.captures![0].id as string;
+      const currencyCode = purchaseUnit.amount?.currencyCode as string;
+      const paymentsController = new PaymentsController(this.paypal_);
+      try {
+        await paymentsController.refundCapturedPayment({
+          captureId: paymentId,
+          body: {
+            amount: {
+              currencyCode: currencyCode,
+              value: (paymentData.amount as BigNumberRawValue).value.toString()
+            }
+          }
+        })
+        return await this.retrievePayment(paymentData)
+      } catch (error) {
+        this.logger_.error(error);
+        throw new Error("An error occurred in refundPayment")
+      }
+    }
+    throw new Error("An error occurred in refundPayment")
+  }
+  async retrievePayment(paymentSessionData: RetrievePaymentInput): Promise<RetrievePaymentOutput> {
+    const ordersController = new OrdersController(this.paypal_);
+    try {
+      const paypalOrderId = (paymentSessionData.data!.paypalOrder as PaypalOrder).id as string
+      const { result, ...httpResponse } = await ordersController.getOrder({
+        id: paypalOrderId
+      })
+      if (result.id) {
+        return {
+          data: {
+            ...paymentSessionData,
+            paypalOrderId: result.id,
+            paypalOrder: result,
+          }
+        }
+      }
+    } catch (error) {
+      if (error instanceof ApiError) {
+        const { statusCode, headers } = error;
+        this.logger_.error(`Status code ${statusCode.toString()}`)
+        this.logger_.error(JSON.stringify(headers))
+        throw new Error("Initialize payment failed")
+      }
+    }
+    throw new Error("Initialize payment failed.")
+  }
+  async updatePayment(context: UpdatePaymentInput): Promise<UpdatePaymentOutput> {
+    const ordersController = new OrdersController(this.paypal_);
+    try {
+      const paypalOrderId = (context.data!.paypalOrder as PaypalOrder).id as string
+      await ordersController.patchOrder({
+        id: paypalOrderId,
+        body: [
+          {
+            op: PatchOp.Replace,
+            value: {
+              amount: {
+                currencyCode: context.currency_code,
+                value: context.amount.toString()
+              }
+            }
+          }
+        ]
+      })
+      return await this.retrievePayment(context)
+    } catch (error) {
+      this.logger_.error(error);
+      throw new Error("An error occurred in updatePayment")
+    }
+  }
+  async getWebhookActionAndData(data: ProviderWebhookPayload["payload"]): Promise<WebhookActionResult> {
+    return {
+      action: PaymentActions.NOT_SUPPORTED
+    }
+  }
+}
+
+export default PaypalPaymentProviderService
